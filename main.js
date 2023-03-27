8ea6e6fe73e564e4d353e9b5fcf8f0f2f6fd359f
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-03-27 15:48:49 -0700

    Update main.js

--- main.js
+++ main.js
@@ -37,3 +37,17 @@ function sortEmotions(arr, order){
     })
   }
 }
+
+
+/*
+
+better! 
+
+function sortEmotions(arr, order){
+  const emotions = { ':D': 1, ':)': 2, ':|': 3, ':(': 4, 'T_T': 5 }
+  arr = arr.sort((a, b) => emotions[a] - emotions[b]);
+  return order && arr || arr.reverse();
+}
+
+
+*/
