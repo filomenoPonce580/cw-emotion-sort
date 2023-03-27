8d1791b4309885e6160553e59e55b59acf8b2f4f
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-03-27 15:46:58 -0700

    Create main.js
    first attempt

---
+++ main.js
@@ -0,0 +1,39 @@
+function sortEmotions(arr, order){
+  const keyNums = {
+    '1': ":D",
+    '2': ":)",
+    '3': ":|",
+    '4': ":(",
+    '5': "T_T"
+  }
+  
+    const keySmiles = {
+    ":D": '1',
+    ":)": '2',
+    ":|": '3',
+    ":(": '4',
+    "T_T": '5'
+  }
+  
+  let emoteToNums = arr.map((emotion)=>{
+      for(key in keySmiles){
+        return keySmiles[emotion]
+      }       
+    })
+
+  if(order === true){
+    return emoteToNums.sort().map((number)=>{
+        for(key in keyNums){
+          return keyNums[number]
+        }       
+    })
+  }else{
+    return emoteToNums
+      .sort((a,b)=>b-a)
+      .map((number)=>{
+        for(key in keyNums){
+          return keyNums[number]
+        }       
+    })
+  }
+}
